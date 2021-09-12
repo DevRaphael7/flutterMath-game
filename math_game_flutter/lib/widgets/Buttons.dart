7460a4170e38f26ed6d8c8748426ff2056d362fe
DevRaphael7 <raphaelramalho1989@gmail.com>
2021-09-12 17:12:31 -0300

    Refatorando o código

--- math_game_flutter/lib/widgets/Buttons.dart
+++ math_game_flutter/lib/widgets/Buttons.dart
@@ -4,41 +4,6 @@ import 'dart:math';
 import 'package:math_game_flutter/class/Quiz.dart';
 import 'Functions.dart';
 
-class ButtonJogar extends StatelessWidget{
-
-  String input1;
-  BuildContext context;
-  
-  @override 
-  Widget build(BuildContext context){
-    return ButtonTheme(
-              buttonColor: Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30)
-              ),
-              minWidth: 200,
-              highlightColor: Color(0xFF8604f9),
-              height: 50,
-              child: RaisedButton(
-                child: Text( "Jogar",
-                  style: TextStyle(
-                    color: Color(0xff8604f9),
-                    fontWeight: FontWeight.bold,
-                  ),),
-                onPressed: () {
-                  if( passaParaProximaPagina(input1) ) {
-                    passarParaOutraTela(context, "/quiz", input: input1);
-                  }
-                }
-              ),
-            );
-  }
-
-  ButtonJogar(String input1){
-    this.input1 = input1;
-  }
-}
-
 class HomePageButton extends StatelessWidget{
 
   @override 
